1f7403dd29f6d8ba436cac65669bb39182bd5f6e
Adam Mężydło <adamkmezydlo@gmail.com>
2024-04-08 01:13:16 +0200

    small bug fix

--- src/drivers/insolation.py
+++ src/drivers/insolation.py
@@ -3,14 +3,14 @@ from features.analog_accessor.analog_accessor import AnalogAccessor
 
 class InsolationDriver:
     def __init__(self, analog_accessor: AnalogAccessor, channel, min_insolation=0, max_insolation=65535):
-        self.accessor  = analog_accessor
+        self.analog_accessor  = analog_accessor
         self.__insolation = -1
         self.__channel = channel
         self.__min_insolation = min_insolation
         self.__max_insolation = max_insolation
 
     def measure(self):
-        self.__insolation = analog_accessor.read(self.__channel)
+        self.__insolation = self.analog_accessor.read(self.__channel)
 
 
     def insolation(self):
--- src/drivers/sms.py
+++ src/drivers/sms.py
@@ -2,14 +2,14 @@ from features.analog_accessor.analog_accessor import AnalogAccessor
 
 class SMSDriver:
     def __init__(self, analog_accessor: AnalogAccessor, channel, min_moist=0, max_moist=65535):
-        self.accessor  = analog_accessor
+        self.analog_accessor  = analog_accessor
         self.__moisture = -1
         self.__channel = channel
         self.__min_moist = min_moist
         self.__max_moist = max_moist
 
     def measure(self):
-        self.__moisture = analog_accessor.read(self.__channel)
+        self.__moisture = self.analog_accessor.read(self.__channel)
 
 
     def moisture(self):
