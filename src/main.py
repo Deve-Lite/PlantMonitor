e5a4e96cf7f7846e41acecddb1b1065251e2c0f0
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2024-05-06 20:17:13 +0200

    feat(documentation setup)

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import sys
 import gc
 import uasyncio
 import ntptime
@@ -6,8 +5,7 @@ import ntptime
 from application import App
 from features.analog_accessor.analog_accessor_factory import AnalogAccessorFactory
 from features.devices.device_factory import DeviceFactory
-from features.logger.file_logger import FileLogger
-from features.logger.logger_levels import LoggerLevels
+from features.logger.file_logger import FileLogger, Logger
 from features.mqtt.mqtt_factory import MqttFactory
 from features.network.connection_factory import ConnectionFactory
 
@@ -15,51 +13,56 @@ from utime import sleep, localtime, gmtime
 from machine import reset, Pin
 
 
-def setup_fail(logger: Logger, message: str, error_code: int):
+def setup_fail(logger: Logger, message: str):
     logger.error(message)
     sleep(5)
-    sys.exit(error_code)
-    # in release change to reset()
+    reset()
 
 
-def blink():
-    led.value(1)
-    sleep(0.2)
-    led.value(0)
+def blink(led, n=1):
+    for i in range(n):
+        led.value(1)
+        sleep(0.2)
+        led.value(0)
+        sleep(0.2)
 
 
 if __name__ == '__main__':
-    gc.collect()
+    try:
+        gc.collect()
 
-    logger = FileLogger(console=True, debug=False)
-    logger.info("Device starting")
-    led = Pin(25, Pin.OUT)
-    led.value(0)
-    sleep(1)
+        logger = FileLogger(console=False, debug=False)
+        logger.info("Device starting")
+        led = Pin(25, Pin.OUT)
+        led.value(0)
+        sleep(1)
 
-    connection = ConnectionFactory(logger).create()
-    connection_result = connection.connect()
-    if not connection_result:
-        setup_fail(logger, f"Failed to connect with {connection.config.type}.", 1)
+        blink(led, 1)
+        connection = ConnectionFactory(logger).create()
+        connection_result = connection.connect()
+        if not connection_result:
+            setup_fail(logger, f"Failed to connect with {connection.config.type}.", 1)
 
-    logger.info("Synchronizing time with ntptime module")
-    ntptime.settime()
-    logger.info(f"Global time: {gmtime()}")
+        logger.info("Synchronizing time with ntptime module")
+        blink(led, 2)
+        ntptime.settime()
+        logger.info(f"Global time: {gmtime()}")
 
-    mqtt = MqttFactory(logger).create()
-    mqtt_connection_result = mqtt.connect()
+        blink(led, 3)
+        mqtt = MqttFactory(logger).create()
+        mqtt_connection_result = mqtt.connect()
 
-    if not mqtt_connection_result:
-        setup_fail(logger, f"Failed to connect with {mqtt.config.type}.", 2)
+        if not mqtt_connection_result:
+            setup_fail(logger, f"Failed to connect with {mqtt.config.type}.", 2)
 
-    accessor_factory = AnalogAccessorFactory(logger)
-    accessors = accessor_factory.create()
+        accessor_factory = AnalogAccessorFactory(logger)
+        accessors = accessor_factory.create()
 
-    device_factory = DeviceFactory(mqtt, accessors, logger)
-    devices = device_factory.create()
+        device_factory = DeviceFactory(mqtt, accessors, logger)
+        devices = device_factory.create()
 
-    app = App(mqtt, devices, logger)
-    uasyncio.run(app.start())
-
-
-
+        blink(led, 4)
+        app = App(mqtt, devices, logger)
+        uasyncio.run(app.start())
+    except Exception as e:
+        setup_fail(logger, str(e), 2)
