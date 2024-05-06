f7fcdeb90586a6f1337bd721b1295f7160a1d01c
Łukasz Kolber <kolberu@gmail.com>
2024-05-06 19:17:52 +0200

    feat(reset setup)

--- src/application.py
+++ src/application.py
@@ -2,11 +2,10 @@ from features.devices.device import Device
 from features.logger.logger import Logger
 from features.mqtt.mqtt import BaseMqttClient
 
+from machine import reset
 from utime import sleep
 
 import uasyncio
-import _thread
-import gc
 
 
 async def message_loop(logger: Logger, mqtt: BaseMqttClient):
@@ -21,7 +20,15 @@ class App:
         self._logger = logger
         self._devices = devices
 
+    def reset_devices(self):
+        self._logger.info("Resetting device.")
+        sleep(1)
+        reset()
+
     async def start(self):
+
+        self._mqtt.subscribe("reset", self.reset_devices)
+
         message = uasyncio.create_task(message_loop(self._logger, self._mqtt))
 
         self._logger.debug(f"Starting devices loop: {len(self._devices)}")
--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from application import App
 from features.analog_accessor.analog_accessor_factory import AnalogAccessorFactory
 from features.devices.device_factory import DeviceFactory
 from features.logger.file_logger import FileLogger
-from features.logger.logger_levels import LoggerLevels
 from features.mqtt.mqtt_factory import MqttFactory
 from features.network.connection_factory import ConnectionFactory
 
@@ -18,48 +17,53 @@ from machine import reset, Pin
 def setup_fail(logger: Logger, message: str, error_code: int):
     logger.error(message)
     sleep(5)
-    sys.exit(error_code)
-    # in release change to reset()
+    reset()
 
 
-def blink():
-    led.value(1)
-    sleep(0.2)
-    led.value(0)
+def blink(led, n=1):
+    for i in range(n):
+        led.value(1)
+        sleep(0.2)
+        led.value(0)
+        sleep(0.2)
 
 
 if __name__ == '__main__':
-    gc.collect()
+    try:
+        gc.collect()
 
-    logger = FileLogger(console=True, debug=False)
-    logger.info("Device starting")
-    led = Pin(25, Pin.OUT)
-    led.value(0)
-    sleep(1)
+        logger = FileLogger(console=True, debug=False)
+        logger.info("Device starting")
+        led = Pin(25, Pin.OUT)
+        led.value(0)
+        sleep(1)
 
-    connection = ConnectionFactory(logger).create()
-    connection_result = connection.connect()
-    if not connection_result:
-        setup_fail(logger, f"Failed to connect with {connection.config.type}.", 1)
+        blink(led, 1)
+        connection = ConnectionFactory(logger).create()
+        connection_result = connection.connect()
+        if not connection_result:
+            setup_fail(logger, f"Failed to connect with {connection.config.type}.", 1)
 
-    logger.info("Synchronizing time with ntptime module")
-    ntptime.settime()
-    logger.info(f"Global time: {gmtime()}")
+        logger.info("Synchronizing time with ntptime module")
+        blink(led, 2)
+        ntptime.settime()
+        logger.info(f"Global time: {gmtime()}")
 
-    mqtt = MqttFactory(logger).create()
-    mqtt_connection_result = mqtt.connect()
+        blink(led, 3)
+        mqtt = MqttFactory(logger).create()
+        mqtt_connection_result = mqtt.connect()
 
-    if not mqtt_connection_result:
-        setup_fail(logger, f"Failed to connect with {mqtt.config.type}.", 2)
+        if not mqtt_connection_result:
+            setup_fail(logger, f"Failed to connect with {mqtt.config.type}.", 2)
 
-    accessor_factory = AnalogAccessorFactory(logger)
-    accessors = accessor_factory.create()
+        accessor_factory = AnalogAccessorFactory(logger)
+        accessors = accessor_factory.create()
 
-    device_factory = DeviceFactory(mqtt, accessors, logger)
-    devices = device_factory.create()
+        device_factory = DeviceFactory(mqtt, accessors, logger)
+        devices = device_factory.create()
 
-    app = App(mqtt, devices, logger)
-    uasyncio.run(app.start())
-
-
-
+        blink(led, 4)
+        app = App(mqtt, devices, logger)
+        uasyncio.run(app.start())
+    except Exception as e:
+        setup_fail(logger, str(e), 2)
